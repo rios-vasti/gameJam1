edc8324f079ee3de6278cf64a33b7a3173c0d404
jrea1-trinity <jrea1@trinity.edu>
2021-10-10 17:52:35 -0500

    finished start game screen

---
+++ Assets/Scripts/StartUIController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartUIController : MonoBehaviour
+{
+    public void DisplayRules()
+    {
+        SceneManager.LoadScene("RulesScene", LoadSceneMode.Additive);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("LevelOne", LoadSceneMode.Single);
+    }
+    public void CloseRules()
+    {
+        SceneManager.UnloadSceneAsync("RulesScene");
+    }
+    public void DisplayHighscore()
+    {
+        
+    }
+}
